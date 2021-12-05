27bda25d83ca2cf21ac78bdf4cb117a46d941eea
Khanhlynhne <95580156+Khanhlynhne@users.noreply.github.com>
2021-12-05 22:34:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Hey boy',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^Click vào đây điiiii^^',
+    title: 'Cậu có iu tớ khum 🥰',
+    desc: 'Trả lời thật lòng nha -.- ',
+    btnYes: 'Đúng rùi nè hihi <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Thế giới hơn 7 tỉ người mà sao cậu lại iu tớ <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Tớ biết mà 🥰. Tớ cũng yêu cậu 😘😘',
+    messDesc: 'Hôm nay kết thúc Trái tim tình nguyện 2021 rồi, chúng ta cũng cùng kết thúc chuỗi ngày độc thân nha. Làm người yêu tớ nhé 🥰',
+    btnAccept: 'Nhắn tin lại cho tớ đi nào <3',
+    messLink: 'https://www.facebook.com/keen.2909.
 }
